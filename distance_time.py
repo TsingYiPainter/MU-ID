8e5f5fe7e803768bc4357f34baeb1ade441bba19
wyh <1482173319@qq.com>
2024-12-03 17:55:51 +0800

    fix bug

--- distance_time.py
+++ distance_time.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 
 static_data = utils.parse_tsw1400("./mmwave_3/static.bin", num_chirps_per_frame=34, num_frames=1000, num_ants= 2, num_adc_samples=598)
-adc_data = utils.parse_tsw1400("./mm4/w1.bin", num_chirps_per_frame=34, num_frames=1000, num_ants= 2, num_adc_samples=598)
+adc_data = utils.parse_tsw1400("./mm4/z1.bin", num_chirps_per_frame=34, num_frames=1000, num_ants= 2, num_adc_samples=598)
 
 
 print(adc_data.shape)
@@ -30,33 +30,26 @@ def cal2dFFTByFrame(index,origin2dFFT):
 
     twod_fft = twod_fft-origin2dFFT
     # 搬移 Doppler 频谱
-    detMatrix_shifted = np.fft.fftshift(twod_fft, axes=1)
-
-    # 定义满量程幅值和阈值
-    # full_scale = np.max(detMatrix_shifted)  # 假设最大值为满量程
-    threshold_dBFS = 9  # 阈值 9 dBFS
-
-    # # 计算幅度和 dBFS 值
-    # amplitude = np.abs(detMatrix_shifted)  # 计算幅度
-    # dBFS = 20 * np.log10(amplitude / full_scale)  # 计算 dBFS
+    #detMatrix_shifted = np.fft.fftshift(twod_fft, axes=1)
+    detMatrix_shifted = twod_fft
+    threshold_dBFS = 10  # 阈值 9 dBFS
 
     # 应用阈值
     detMatrix_thresholded = np.where(detMatrix_shifted >= threshold_dBFS, detMatrix_shifted, 0)
     return detMatrix_thresholded
 
 range_resolution = mm.dsp.range_resolution(num_adc_samples=598,dig_out_sample_rate=3000,freq_slope_const=18.829)
-#print("range_resolution is {}m".format(range_resolution[0]))
-print("bandwidth is {} ".format(range_resolution[1]))
-T_chirp= 212.43*1e-6
-T_idle = 300*1e-6
-PRF = 1 / (T_chirp + T_idle)
-doppler_bandwidth = PRF / 2
-print("bandwidth is {} ".format(doppler_bandwidth))
-print(3*10e8/(2*77*10e3*17*512.43))
+print("range_resolution is {}m".format(range_resolution[0]))
+# T_chirp= 212.43*1e-6
+# T_idle = 300*1e-6
+# PRF = 1 / (T_chirp + T_idle)
+# doppler_bandwidth = PRF / 2
+# print("bandwidth is {} ".format(doppler_bandwidth))
+# print(3*10e8/(2*77*10e3*17*512.43))
 
-doppler_resolution = mm.dsp.doppler_resolution(band_width=range_resolution[1] ,start_freq_const=77,ramp_end_time = 212.43,idle_time_const=300,num_loops_per_frame=34,num_tx_antennas=2)
+#doppler_resolution = mm.dsp.doppler_resolution(band_width=range_resolution[1] ,start_freq_const=77,ramp_end_time = 212.43,idle_time_const=300,num_loops_per_frame=34,num_tx_antennas=2)
 #print("doppler_resolution is {}m/s".format(doppler_resolution))
-#doppler_resolution =3*10e8/(2*77*10e3*17*512.43)
+doppler_resolution =3*10e8/(2*77*10e3*17*512.43)
 doppler_bins = np.arange(-num_doppler_bins//2+1, num_doppler_bins//2+1)  # 搬移后的 Doppler Bin 索引
 velocity_axis = doppler_bins * doppler_resolution  # 实际速度轴
 
@@ -69,9 +62,12 @@ origin2dFFT = cal2dFFT0()
 for frame_idx in range(num_frames):
     detMatrix_shifted = cal2dFFTByFrame(frame_idx,origin2dFFT)
     R_hat = detMatrix_shifted / np.max(detMatrix_shifted, axis=1, keepdims=True)
+    R_hat = np.nan_to_num(R_hat, nan=0.0)
+    # print(R_hat)
     # 计算每个 Range Bin 的主导速度 V̂_i
     for i in range(num_range_bins):
-        velocity_map[i, frame_idx] = np.sum(R_hat[i, :] * velocity_axis)/17
+        velocity_map[i, frame_idx] = max(np.sum(R_hat[i, :] * velocity_axis)/17,-np.sum(R_hat[i, :] * velocity_axis)/17)
+        #print(velocity_map[i, frame_idx])
 
 range_bins = np.arange(-num_range_bins//2+1, num_range_bins//2+1)  # 搬移后的 Doppler Bin 索引
 range_bins = range_bins[::-1]
--- distance_velocity.py
+++ distance_velocity.py
@@ -4,9 +4,14 @@ from mmwave.dataloader import utils
 import numpy as np
 import matplotlib.pyplot as plt
 
-adc_data = utils.parse_tsw1400("./mmwave_2/ycwz1.bin", num_chirps_per_frame=17, num_frames=1000, num_ants= 2, num_adc_samples=256)
+static_data = utils.parse_tsw1400("./mmwave_3/static.bin", num_chirps_per_frame=34, num_frames=1000, num_ants= 2, num_adc_samples=598)
+adc_data = utils.parse_tsw1400("./mm4/w1.bin", num_chirps_per_frame=34, num_frames=1000, num_ants= 2, num_adc_samples=598)
 
 
+def cal2dFFT0():
+    radar_cube = mm.dsp.range_processing(static_data[600])
+    detMatrix = mm.dsp.doppler_processing(radar_cube=radar_cube,interleaved=False)
+    return detMatrix
 
 def cal2dFFTByFrame(index):
     radar_cube = mm.dsp.range_processing(adc_data[index])
@@ -14,16 +19,15 @@ def cal2dFFTByFrame(index):
     detMatrix = mm.dsp.doppler_processing(radar_cube=radar_cube,interleaved=False)
     return detMatrix
 
-detMatrix_0 = cal2dFFTByFrame(0)
+detMatrix_0 = cal2dFFT0()
 twod_fft_0 = detMatrix_0[0]
-detMatrix = cal2dFFTByFrame(300)
+detMatrix = cal2dFFTByFrame(200)
 twod_fft = detMatrix[0]
 
-#twod_fft = twod_fft-twod_fft_0
+twod_fft = twod_fft-twod_fft_0
 
-range_resolution = mm.dsp.range_resolution(num_adc_samples=256,dig_out_sample_rate=3000,freq_slope_const=18.829)
+range_resolution = mm.dsp.range_resolution(num_adc_samples=598,dig_out_sample_rate=3000,freq_slope_const=18.829)
 print("range_resolution is {}m".format(range_resolution[0]))
-doppler_resolution = mm.dsp.doppler_resolution(band_width=range_resolution[1] ,start_freq_const=77,ramp_end_time = 200,idle_time_const=100,num_loops_per_frame=17,num_tx_antennas=2)
 doppler_resolution =3*10e8/(2*77*10e3*17*300)
 print("doppler_resolution is {}m/s".format(doppler_resolution))
 
@@ -43,10 +47,14 @@ print(velocity_axis)
 
 # 搬移 Doppler 频谱
 detMatrix_shifted = np.fft.fftshift(twod_fft, axes=1)
+threshold_dBFS = 10  # 阈值 9 dBFS
+
+# 应用阈值
+detMatrix_thresholded = np.where(detMatrix_shifted >= threshold_dBFS, detMatrix_shifted, 0)
 
 # 可视化带物理意义的 Range-Doppler Map
 plt.imshow(
-    detMatrix_shifted,
+    detMatrix_thresholded,
     aspect='auto',
     cmap='coolwarm',
     interpolation='nearest',
